5b79d121295379903e41a80bc974d175781d3a76
Ben Leitl <ben.leitl@me.com>
2020-02-17 10:20:41 +0100

    Add Scanner, array and comparison

--- src/com/codersbay/Main.java
+++ src/com/codersbay/Main.java
@@ -1,5 +1,37 @@
 package com.codersbay;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please input " + numbers.length + " int values!");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Please input int value #" + (i + 1));
+            numbers[i] = scanner.nextInt();
+
+        }
+        System.out.println("Thank you!");
+        System.out.println("You have entered the following numbers:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + ", ");
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println("The largest number of those entered is....");
+
+        int biggestNumber = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > biggestNumber) {
+                biggestNumber = numbers[i];
+            }
+        }
+        System.out.println(biggestNumber);
+
+
+    }
 
 }
